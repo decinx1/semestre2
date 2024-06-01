9ff299948f314c8b3441d8ee136688e43cc9dd74
gustavo <gaguilar7@ucol.mx>
2024-05-31 23:07:43 -0600

    bg practic kda

--- scrip_spam_bot/smapMessage.py
+++ scrip_spam_bot/smapMessage.py
@@ -1,6 +1,9 @@
 import pyautogui as pg
 import time 
 
+#A script was made with which you can spam, it is made up of 2 libraries.
+#PyAutoGUI: lets your Python scripts control the mouse and keyboard to automate interactions with other applications.
+#TIME: time module allows to work with time in Python. It allows functionality like getting the current time, pausing the Program from executing.
 time.sleep(5)
 
 for i in range(100):
